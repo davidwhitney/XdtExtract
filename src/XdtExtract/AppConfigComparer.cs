d7aa20ba4e21407dd34ac4f10349665a0e6832e1
David Whitney <david@davidwhitney.co.uk>
2015-01-17 00:22:02 +0000

    This is a work in progress - everything is broken.

---
+++ src/XdtExtract/AppConfigComparer.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace XdtExtract
+{
+    public class IterateOver
+    {
+        public static IEnumerable<ComparisonPair<XElement>> Pairs(XDocument baseDoc, XDocument comparisonDoc)
+        {
+            var baseKeys = new List<string>();
+
+            foreach (var appSetting in baseDoc.AppSettings())
+            {
+                var sourceSettingKey = appSetting.Attributes().Key();
+                var comparisonSetting = comparisonDoc.AppSettings().SettingOrDefault(sourceSettingKey);
+
+                baseKeys.Add(sourceSettingKey);
+
+                yield return new ComparisonPair<XElement>(appSetting, comparisonSetting);
+            }
+
+            foreach (var otherSetting in comparisonDoc.AppSettings())
+            {
+                var key = otherSetting.Attributes().Key();
+
+                if (!baseKeys.Contains(key))
+                {
+                   yield return new ComparisonPair<XElement>(null, otherSetting); 
+                }
+            }
+        }
+    }
+
+    public class ComparisonPair<T>
+    {
+        public T BaseItem { get; set; }
+        public T ComparisonItem { get; set; }
+
+        public ComparisonPair(T baseItem, T comparisonItem)
+        {
+            BaseItem = baseItem;
+            ComparisonItem = comparisonItem;
+        }
+    }
+
+    public class AppConfigComparer
+    {
+        public IEnumerable<Diff> Compare(string baseConfig, string comparisonConfig)
+        {
+            var baseDoc = XDocument.Parse(baseConfig);
+            var comparisonDoc = XDocument.Parse(comparisonConfig); 
+            
+            var diffs = new List<Diff>();
+
+            foreach (var settingPair in IterateOver.Pairs(baseDoc, comparisonDoc))
+            {
+                if (settingPair.ComparisonItem == null)
+                {
+                    diffs.Add(new Diff
+                    {
+                        Operation = Operation.Remove,
+                        XPath = "/configuration/appSettings/add[@key=\""+settingPair.BaseItem+"\"]"
+                    });
+                }
+
+                foreach (var attribute in settingPair.BaseItem.Attributes())
+                {
+                    
+                }
+
+                Debug.WriteLine(settingPair.BaseItem.Name);
+            }
+
+            return diffs;
+        }
+    }
+
+    public static class XDocumentExtensions
+    {
+        public static IEnumerable<XElement> AppSettings(this XDocument src)
+        {
+            return src.Descendants().Where(x => x.Name == "appSettings").Descendants();
+        }
+
+        public static XElement SettingOrDefault(this IEnumerable<XElement> src, string key)
+        {
+            return src.SingleOrDefault(x => x.Attributes().Single(attr => attr.Name == "key").Value == key);
+        } 
+
+        public static string Key(this IEnumerable<XAttribute> src)
+        {
+            return src.Single(x => x.Name == "key").Value;
+        } 
+    }
+
+    public class Diff
+    {
+        public string XPath { get; set; }
+        public Operation Operation { get; set; }
+    }
+
+    public enum Operation
+    {
+        Add,
+        Remove,
+        Modify
+    }
+}
---
+++ tests/XdtExtract.Test.Unit/AppConfigComparerTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace XdtExtract.Test.Unit
+{
+    [TestFixture]
+    public class AppConfigComparerTests
+    {
+        private AppConfigComparer _comparer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _comparer = new AppConfigComparer();
+        }
+
+        [Test]
+        public void DetectChanges_WithAppConfigContainingSingleAppSettingThatGetsRemoved_ReturnsChange()
+        {
+            var c1 = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""Value"" value=""true"" />
+  </appSettings>
+</configuration>";
+
+            var c2 = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+  </appSettings>
+</configuration>";
+            
+            var diffs = _comparer.Compare(c1, c2).ToList();
+
+            Assert.That(diffs[0].XPath, Is.EqualTo(@"/configuration/appSettings/add[@key='Value']"));
+        }
+
+        [Test]
+        public void DetectChanges_WithAppConfigContainingSingleAppSettingDifference_ReturnsChange()
+        {
+            var c1 = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""Value"" value=""true"" />
+  </appSettings>
+</configuration>";
+
+            var c2 = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""Value"" value=""false"" />
+  </appSettings>
+</configuration>";
+            
+            var diffs = _comparer.Compare(c1, c2).ToList();
+
+            Assert.That(diffs[0].XPath, Is.EqualTo(@"/configuration/appSettings/add[@key='Value']"));
+        }
+    }
+}
