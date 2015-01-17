d7de4385e8d2eb48a6ad1bac3bb5f052cf7502d8
David Whitney <david@davidwhitney.co.uk>
2015-01-17 00:53:15 +0000

    Cleaned up nasty xml repetition in tests

--- tests/XdtExtract.Test.Unit/AppConfigComparerTests.cs
+++ tests/XdtExtract.Test.Unit/AppConfigComparerTests.cs
@@ -22,20 +22,10 @@ namespace XdtExtract.Test.Unit
         [Test]
         public void DetectChanges_WithAppConfigContainingSingleAppSettingThatGetsRemoved_ReturnsChange()
         {
-            var c1 = @"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-  <appSettings>
-    <add key=""Value"" value=""true"" />
-  </appSettings>
-</configuration>";
-
-            var c2 = @"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-  <appSettings>
-  </appSettings>
-</configuration>";
+            var @base = ConfigWithSettings(@"<add key=""Value"" value=""true"" />");
+            var comparison = ConfigWithSettings();
             
-            var diffs = _comparer.Compare(c1, c2).ToList();
+            var diffs = _comparer.Compare(@base, comparison).ToList();
 
             Assert.That(diffs[0].XPath, Is.EqualTo(@"/configuration/appSettings/add[@key='Value']"));
             Assert.That(diffs[0].Operation, Is.EqualTo(Operation.Remove));
@@ -44,20 +34,10 @@ namespace XdtExtract.Test.Unit
         [Test]
         public void DetectChanges_WithDestinationAppSettingContainsSingleAddition_ReturnsChange()
         {
-            var c1 = @"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-  <appSettings>
-  </appSettings>
-</configuration>";
-
-            var c2 = @"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-  <appSettings>
-    <add key=""Value"" value=""true"" />
-  </appSettings>
-</configuration>";
+            var @base = ConfigWithSettings();
+            var comparison = ConfigWithSettings(@"<add key=""Value"" value=""true"" />");
             
-            var diffs = _comparer.Compare(c1, c2).ToList();
+            var diffs = _comparer.Compare(@base, comparison).ToList();
 
             Assert.That(diffs[0].XPath, Is.EqualTo(@"/configuration/appSettings/add[@key='Value']"));
             Assert.That(diffs[0].Operation, Is.EqualTo(Operation.Add));
@@ -66,23 +46,28 @@ namespace XdtExtract.Test.Unit
         [Test]
         public void DetectChanges_WithAppConfigContainingSingleAppSettingDifference_ReturnsChange()
         {
-            var c1 = @"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-  <appSettings>
-    <add key=""Value"" value=""true"" />
-  </appSettings>
-</configuration>";
-
-            var c2 = @"<?xml version=""1.0"" encoding=""utf-8""?>
-<configuration>
-  <appSettings>
-    <add key=""Value"" value=""false"" />
-  </appSettings>
-</configuration>";
+            var @base = ConfigWithSettings(@"<add key=""Value"" value=""true"" />");
+            var comparison = ConfigWithSettings(@"<add key=""Value"" value=""false"" />");
             
-            var diffs = _comparer.Compare(c1, c2).ToList();
+            var diffs = _comparer.Compare(@base, comparison).ToList();
 
             Assert.That(diffs[0].XPath, Is.EqualTo(@"/configuration/appSettings/add[@key='Value']"));
         }
+
+
+        private static string ConfigWithSettings(string xml = "")
+        {
+            return ConfigWithSettings(new[] { xml });
+        }
+
+        private static string ConfigWithSettings(string[] xml)
+        {
+            return @"<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>" + string.Join(Environment.NewLine, xml) +
+                   @"  </appSettings>
+</configuration>";
+        }
+    
     }
 }
